6b3d9996ae198d9f05a1783a3320bf261af430f7
Krishna <krishna92.48sharma@gmail.com>
2023-08-17 00:42:08 +0530

    Attached DB to save data

---
+++ database/migrations/2023_08_16_185902_create_employee_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id('employee_id');
+            $table->string('name',20);
+            $table->string('dept',10);
+            $table->string('city',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
